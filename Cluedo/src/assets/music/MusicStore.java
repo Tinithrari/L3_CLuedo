51020c8f91e9256fecfe8e0f82cd7b2c726c52aa
xavierheugue <xavierheugue@192.168.0.11>
2015-12-05 18:43:54 +0100

    Ajout du SoundStore et du MusicStore

---
+++ Cluedo/src/assets/music/MusicStore.java
@@ -0,0 +1,32 @@
+package assets.music;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.jsfml.audio.Music;
+
+public class MusicStore {
+private static HashMap<String, Music> musicStore;
+	
+	public static Music getMusic(String key)
+	{
+		Music m;
+		
+		if (musicStore == null)
+			musicStore = new HashMap<String, Music>();
+		
+		if ( (m = musicStore.get(key)) == null)
+		{
+			m = new Music();
+			
+			try {
+				m.openFromStream(m.getClass().getResourceAsStream("assets/music/" + key));
+				musicStore.put(key, m);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return m;
+	}
+}
---
+++ Cluedo/src/assets/sound/SoundStore.java
@@ -0,0 +1,33 @@
+package assets.sound;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.jsfml.audio.SoundBuffer;
+
+public class SoundStore {
+	private static HashMap<String, SoundBuffer> soundStore;
+	
+	public static SoundBuffer getSound(String key)
+	{
+		SoundBuffer buffer;
+		
+		if (soundStore == null)
+			soundStore = new HashMap<String, SoundBuffer>();
+		
+		if ( (buffer = soundStore.get(key)) == null)
+		{
+			buffer = new SoundBuffer();
+			
+			try {
+				buffer.loadFromStream(buffer.getClass().getResourceAsStream("assets/sound/" + key));
+				soundStore.put(key, buffer);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return buffer;
+	}
+}
